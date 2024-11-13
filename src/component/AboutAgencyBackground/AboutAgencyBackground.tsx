5ea0f46c6cb25ef8c3b2b5514ad274f071f70158
EternalSyntaxError <eternal_syntax_error@mail.ru>
2024-11-13 20:03:30 +0500

    v1.2.1
    Добавление анимаций на вторую страницу "PageAboutAgency".

--- src/component/AboutAgencyBackground/AboutAgencyBackground.tsx
+++ src/component/AboutAgencyBackground/AboutAgencyBackground.tsx
@@ -1,47 +1,115 @@
 import type { FC } from "react";
 
+import { motion } from "framer-motion";
+
 import "./AboutAgencyBackground.scss";
 
+
 const AboutAgencyBackground: FC = () => {
-    return (
-        <div className="AboutAgencyBackground">
+  return (
+    <div className="AboutAgencyBackground">
 
-            <div className="TopLine">
-                <div className="Rectangle-1"></div>
+      <div className="TopLine">
+        <motion.div
+          initial={{ height: 0 }}
+          animate={{ height: "100%" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="Rectangle-1">
+        </motion.div>
 
-                <div className="spacer"></div>
+        <div className="spacer"></div>
 
-                <div className="ComboElement">
-                    <div className="RoundedRectangle-1"></div>
-                    <div className="RoundedRectangle-2"></div>
-                    <div className="RoundedRectangle-3">
-                        <div className="RoundedRectangle-4"></div>
-                    </div>
-                </div>
+        <div className="ComboElement">
 
-                <div className="spacer"></div>
+          <motion.div
+            initial={{ y: -200 }}
+            animate={{ y: -100 }}
+            transition={{
+              type: "spring",
+              duration: 1.8,
+              bounce: 0.8,
+            }}
+            className="RoundedRectangle-1">
+          </motion.div>
 
-                <div className="Rectangle-2"></div>
 
-            </div>
+          <motion.div
+            initial={{ y: -200 }}
+            animate={{ y: -50 }}
+            transition={{
+              type: "spring",
+              duration: 2,
+              bounce: 0.6,
+            }}
+            className="RoundedRectangle-2">
+          </motion.div>
 
 
-            <div className="BottomLine">
-                <div className="Сircle-1">
-                    <div className="Сircle-2"></div>
-                </div>
-
-                <div className="spacer"></div>
-
-                <div className="ItemRight">
-                    <div className="Сircle-3"></div>
-                    <div className="RoundedRectangle-5"></div>
-                </div>
-
-            </div>
-
+          <motion.div
+            initial={{ y: -200 }}
+            animate={{ y: -70 }}
+            transition={{
+              type: "spring",
+              duration: 1.2,
+              bounce: 0.7,
+            }}
+            className="RoundedRectangle-3">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut", delay: 1 }}
+              className="RoundedRectangle-4">
+            </motion.div>
+          </motion.div>
         </div>
-    );
+
+        <div className="spacer"></div>
+
+        <motion.div
+          initial={{ height: 0 }}
+          animate={{ height: "100%" }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="Rectangle-2">
+        </motion.div>
+
+      </div>
+
+
+      <div className="BottomLine">
+        <motion.div
+          initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
+          animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+          className="Сircle-1">
+          <motion.div
+            initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
+            animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className="Сircle-2">
+          </motion.div>
+        </motion.div>
+
+        <div className="spacer"></div>
+
+        <div className="ItemRight">
+          <motion.div
+            initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
+            animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
+            transition={{ duration: 1.8, ease: "easeInOut" }}
+            className="Сircle-3">
+          </motion.div>
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: "100%" }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className="RoundedRectangle-5">
+          </motion.div>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
 
 export default AboutAgencyBackground;
--- src/page/PageAboutAgency/PageAboutAgency.tsx
+++ src/page/PageAboutAgency/PageAboutAgency.tsx
@@ -5,43 +5,81 @@ import "./PageAboutAgency.scss";
 import { AboutAgencyBackground } from "../../component/AboutAgencyBackground";
 
 import { ReactSVG } from "react-svg";
+import { motion } from "framer-motion";
+
+
+const containerUl = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 1.5,
+      staggerChildren: 0.3
+    }
+  }
+};
+
+const itemLi = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 
 const PageAboutAgency: FC = () => {
-    return (
-        <div className="PageAboutAgency">
+  return (
+    <div className="PageAboutAgency">
 
-            <div className="InfoCompany">
-                <div className="text">
-                    <h2>Happy</h2>
-                    <p>Агентство цифрового маркетинга, специализирующееся на быстром достижении высоких результатов.</p>
-                </div>
-                <ReactSVG src="./icons/buble-4.svg" />
-            </div>
-
-            <div className="MainInfo">
-                <div className="Сorner-1">
-                    <div className="line-1"></div>
-                    <div className="line-2"></div>
-                </div>
-
-                <div className="text">
-                    <ol>
-                        <li>Инновации: мы всегда стремимся быть на шаг впереди, используя новейшие инструменты и подходы для достижения ваших целей.</li>
-                        <li>Качество: для нас важно, чтобы каждый проект был выполнен на высшем уровне, поэтому мы тщательно контролируем все процессы.</li>
-                        <li>Командная работа: взаимодействие внутри команды и с клиентами – основа нашего успеха. Мы ценим мнение каждого участника процесса.</li>
-                        <li>Результат: наша главная цель – достижение конкретных результатов, которые помогут вашему бизнесу расти и развиваться.</li>
-                    </ol>
-                </div>
-
-                <div className="Сorner-2">
-                    <div className="line-1"></div>
-                    <div className="line-2"></div>
-                </div>
-            </div>
-
-            <AboutAgencyBackground />
+      <motion.div
+        initial={{ opacity: 0, clipPath: "inset(0% 100% 0% 0%)", scale: 0.8 }}
+        animate={{ opacity: 1, clipPath: "inset(-50% -50% -50% -50%)", scale: 1 }}
+        transition={{ type: "spring", stiffness: 100, damping: 10, duration: 5, ease: "easeInOut", delay: 0.8 }}
+        className="InfoCompany">
+        <div className="text">
+          <h2>Happy</h2>
+          <p>Агентство цифрового маркетинга, специализирующееся на быстром достижении высоких результатов.</p>
         </div>
-    );
+        <ReactSVG src="./icons/buble-4.svg" />
+      </motion.div>
+
+      <div className="MainInfo">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut", delay: 0.8 }}
+          className="Сorner-1">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+        </motion.div>
+
+        <div className="text">
+          <motion.ol
+            variants={containerUl}
+            initial="hidden"
+            animate="visible">
+            <motion.li variants={itemLi}>Инновации: мы всегда стремимся быть на шаг впереди, используя новейшие инструменты и подходы для достижения ваших целей.</motion.li>
+            <motion.li variants={itemLi}>Качество: для нас важно, чтобы каждый проект был выполнен на высшем уровне, поэтому мы тщательно контролируем все процессы.</motion.li>
+            <motion.li variants={itemLi}>Командная работа: взаимодействие внутри команды и с клиентами – основа нашего успеха. Мы ценим мнение каждого участника процесса.</motion.li>
+            <motion.li variants={itemLi}>Результат: наша главная цель – достижение конкретных результатов, которые помогут вашему бизнесу расти и развиваться.</motion.li>
+          </motion.ol>
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut", delay: 0.8 }}
+          className="Сorner-2">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+        </motion.div>
+      </div>
+
+      <AboutAgencyBackground />
+    </div>
+  );
 }
 
 export default PageAboutAgency;
