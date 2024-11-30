24c4e94f64e75c40346bc6e132412e77bbe0bc75
EternalSyntaxError <eternal_syntax_error@mail.ru>
2024-11-30 14:37:46 +0500

    v.1.7.1
    Улучшение визуальной составляющей первой страницы "IndexPage".

--- src/component/IndexBackground/IndexBackground.tsx
+++ src/component/IndexBackground/IndexBackground.tsx
@@ -2,20 +2,21 @@ import type { FC } from "react";
 
 import "./IndexBackground.scss";
 
+import { ReactSVG } from "react-svg";
 import { motion } from "framer-motion";
 
+import { CurvedLine } from "../CurvedLine";
 
 const IndexBacground: FC = () => {
   return (
     <div className="IndexBacground">
       <div className="TopLine">
-
         <motion.div
           initial={{ height: 0 }}
           animate={{ height: "100%" }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="Rectangle-1">
-        </motion.div>
+          className="Rectangle-1"
+        ></motion.div>
 
         <div className="spacer"></div>
 
@@ -23,18 +24,69 @@ const IndexBacground: FC = () => {
           initial={{ height: 0 }}
           animate={{ height: "100%" }}
           transition={{ duration: 3, ease: "easeInOut" }}
-          className="Rectangle-2">
+          className="Rectangle-2"
+        >
+          <div className="containerForCross">
+            <motion.div
+              initial={{ scale: 0.3, opacity: 0, rotate: 180 }}
+              animate={{ scale: 1, opacity: 1, rotate: 0 }}
+              transition={{
+                type: "spring",
+                duration: 3,
+                bounce: 0.6,
+                delay: 1.8,
+              }}
+            >
+              <ReactSVG src="/icons/cross.svg" />
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0.3, opacity: 0, rotate: 220 }}
+              animate={{ scale: 1, opacity: 1, rotate: 0 }}
+              transition={{
+                type: "spring",
+                duration: 3,
+                bounce: 0.6,
+                delay: 2,
+              }}
+            >
+              <ReactSVG src="/icons/cross.svg" />
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0.3, opacity: 0, rotate: 240 }}
+              animate={{ scale: 1, opacity: 1, rotate: 0 }}
+              transition={{
+                type: "spring",
+                duration: 3,
+                bounce: 0.6,
+                delay: 2.2,
+              }}
+            >
+              <ReactSVG src="/icons/cross.svg" />
+            </motion.div>
+          </div>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 2 }}
+            className="MiniCircle-2"
+          />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 2.2 }}
+            className="MiniCircle-1"
+          />
         </motion.div>
 
         <div className="spacer"></div>
-
 
         <motion.div
           initial={{ height: 0 }}
           animate={{ height: "100%" }}
           transition={{ duration: 1.5, ease: "easeInOut" }}
-          className="RoundedRectangle-3">
-        </motion.div>
+          className="RoundedRectangle-3"
+        ></motion.div>
 
         <div className="spacer"></div>
 
@@ -42,7 +94,14 @@ const IndexBacground: FC = () => {
           initial={{ height: 0 }}
           animate={{ height: "100%" }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="Rectangle-4">
+          className="Rectangle-4"
+        >
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 2.2 }}
+            className="MiniCircle-3"
+          />
         </motion.div>
 
         <div className="spacer"></div>
@@ -51,9 +110,15 @@ const IndexBacground: FC = () => {
           initial={{ height: 0 }}
           animate={{ height: "100%" }}
           transition={{ duration: 3, ease: "easeInOut" }}
-          className="RoundedRectangle-5">
+          className="RoundedRectangle-5"
+        >
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeOut", delay: 2.2 }}
+            className="MiniCircle-4"
+          />
         </motion.div>
-
       </div>
 
       <div className="BottomLine">
@@ -61,13 +126,14 @@ const IndexBacground: FC = () => {
           initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
           animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="Сircle-1">
+          className="Сircle-1"
+        >
           <motion.div
             initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
             animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
             transition={{ duration: 2, ease: "easeInOut" }}
-            className="Сircle-2">
-          </motion.div>
+            className="Сircle-2"
+          ></motion.div>
         </motion.div>
 
         <div className="spacer"></div>
@@ -77,26 +143,30 @@ const IndexBacground: FC = () => {
             initial={{ clipPath: "inset(0% 100% 0% 0%)" }}
             animate={{ clipPath: "inset(-50% -50% -50% -50%)" }}
             transition={{ duration: 2, ease: "easeInOut" }}
-            className="Сircle-3">
-          </motion.div>
+            className="Сircle-3"
+          ></motion.div>
 
           <motion.div
             initial={{ width: 0 }}
             animate={{ width: "100%" }}
             transition={{ duration: 3.5, ease: "easeInOut" }}
-            className="Rectangle-1">
-          </motion.div>
+            className="Rectangle-1"
+          ></motion.div>
 
           <motion.div
-            initial={{ height: 0 }} animate={{ height: "100%" }} transition={{ duration: 1 }}
-            className="Rectangle-2">
-          </motion.div>
-
+            initial={{ height: 0 }}
+            animate={{ height: "100%" }}
+            transition={{ duration: 1 }}
+            className="Rectangle-2"
+          ></motion.div>
         </div>
-      </div>
 
+        <motion.div className="containerForCurvedLine">
+          <CurvedLine />
+        </motion.div>
+      </div>
     </div>
   );
-}
+};
 
 export default IndexBacground;
--- src/page/IndexPage/IndexPage.tsx
+++ src/page/IndexPage/IndexPage.tsx
@@ -2,34 +2,36 @@ import type { FC } from "react";
 
 import "./IndexPage.scss";
 
-import { ReactSVG } from "react-svg";
 import { motion } from "framer-motion";
+import { ReactSVG } from "react-svg";
 import { Link } from "react-router-dom";
 
 import { IndexBacground } from "../../component/IndexBackground";
 
-const RRD_link = motion.create(Link);
+const LinkElement = motion.create(Link);
 
 const IndexPage: FC = () => {
   return (
     <div className="IndexPage">
-
       <motion.header
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
-        className="MainHeader">
+        className="MainHeader"
+      >
         <h1 className="title">Happy</h1>
         <motion.p
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
-          className="typeCompany">
+          className="typeCompany"
+        >
           Digital-agency
         </motion.p>
       </motion.header>
 
-      <motion.div className="Schedule"
+      <motion.div
+        className="Schedule"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
@@ -42,7 +44,8 @@ const IndexPage: FC = () => {
         </div>
       </motion.div>
 
-      <motion.div className="Order"
+      <motion.div
+        className="Order"
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
@@ -58,22 +61,23 @@ const IndexPage: FC = () => {
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         transition={{ duration: 1.5, ease: "easeInOut" }}
-        className="container-for-next">
-        <RRD_link
+        className="container-for-next"
+      >
+        <LinkElement
           to="/about_agency"
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.8 }}
           transition={{ type: "spring", duration: 1, bounce: 0.7 }}
-          className="NextButton">
+          className="NextButton"
+        >
           <p>Next</p>
-        </RRD_link>
+        </LinkElement>
       </motion.div>
 
       <IndexBacground />
 
     </div>
   );
-}
-
+};
 
 export default IndexPage;
